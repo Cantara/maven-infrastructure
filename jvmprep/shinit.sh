3fb4f356120cefbf54f65dd5faa7b20edab80ac1
Thor Henning Hetland <totto@totto.org>
2015-12-05 14:02:11 +0100

    updated doc

--- jvmprep/shinit.sh
+++ jvmprep/shinit.sh
@@ -1,4 +1,4 @@
-f [ -f $JRE/lib/net.properties ]; then
+if [ -f $JRE/lib/net.properties ]; then
   dockerRoute=$(route | grep '*')
   dockerNet=${dockerRoute/\.0.*/.*}
   sed -i $JRE/lib/net.properties -e "s/nonProxyHosts=localhost|/nonProxyHosts=${dockerNet}|localhost|/"
